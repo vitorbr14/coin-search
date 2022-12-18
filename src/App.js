26433a6d37dea33840bc4674ac0a29537a1c6725
Vitor G <vitorgomes605@gmail.com>
2022-12-18 19:18:30 -0300

    Páginas dinâmicas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Coins from './components/Coins'
 import Navbar from './components/Navbar'
+import {Routes, Route} from 'react-router-dom'
+import Coin from '../src/routes/Coin'
+
 
 function App() {
 
   const [coins, setCoins] = useState([])
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-  
+   console.log('esta é a variável coins' + coins)
   useEffect(()=>{
     axios.get(url).then((response)=> {
       setCoins(response.data)
@@ -19,7 +22,14 @@ function App() {
   return (
     <>
     <Navbar />
-    <Coins coins={coins} />
+    <Routes>
+      <Route path='/' element={<Coins coins={coins} />} />
+      <Route path='/coin' element={<Coin />}>
+        <Route path=":coinId" element={<Coin />}/>
+
+      </Route>
+    </Routes>
+    
     </>
   );
 }
